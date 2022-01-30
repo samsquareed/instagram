ffa6ad3c52620be9d84e58cb1716d466f17b8216
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-01-30 20:43:55 +0530

    changes

--- backend_server/keys.js
+++
@@ -1,4 +0,0 @@
-// module.exports ={
-//     MONGOURI : "mongodb+srv://samsquare:sammed1234@cluster0.kmtdr.mongodb.net/instagramDb?retryWrites=true&w=majority",
-//     JWT_SECRET :"momanddad"
-// }
