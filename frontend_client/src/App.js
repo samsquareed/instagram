8913e3222472c4154d114b8cca051b031eb2c0fd
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-01-31 19:12:37 +0530

    way of adding react context API

--- frontend_client/src/App.js
+++ frontend_client/src/App.js
@@ -1,18 +1,24 @@
 import Navbar from './components/Navbar';
 import "./App.css"
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
 import Home from "./components/screens/Home"
 import Login from "./components/screens/Login"
 import Signup from "./components/screens/Signup"
 import Profile from "./components/screens/Profile"
 import CreatePost from "./components/screens/CreatePost"
+import { createContext, useReducer } from 'react';
+
+import {initialstate, reducer} from './reducers/userReducer'
+
+const UserContext = createContext();
+
+const Routing = ()=>{
 
 
-function App() {    
+    const nevigate = useNavigate();
+
     return(
-        <BrowserRouter>
-        <Navbar />
         <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
@@ -20,7 +26,20 @@ function App() {
             <Route path="/profile" element={<Profile />} />
             <Route path="/createpost" element={<CreatePost />} />
         </Routes>
-        </BrowserRouter>
+    )
+}
+
+function App() {    
+
+    const [state, dispatch] = useReducer(reducer, initialstate)
+
+    return(
+        <UserContext.Provider value={{state, dispatch}}>
+            <BrowserRouter>
+                <Navbar />
+                <Routing />
+            </BrowserRouter>
+        </UserContext.Provider>
     )
 }
 
