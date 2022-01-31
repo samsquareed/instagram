378213b1ea17f52659cc820e2a0b651a3e81c5be
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-01-31 23:46:58 +0530

    beautifully done with likes and unlikes API both frontend and backend

--- frontend_client/src/components/screens/Home.jsx
+++ frontend_client/src/components/screens/Home.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
+import {UserContext} from '../../App'
 import img from '../assets/images/deekshi.jpg'
 import Axios from 'axios'
 
 const Home = () =>{
 
     const [data, setData] = useState([]);
+    const {state,dispatch} = useContext(UserContext)
     useEffect(()=>{
         const authAxios = Axios.create({
             baseURL : 'http://localhost:3001',
@@ -14,11 +16,57 @@ const Home = () =>{
         });
         authAxios.get('/allposts')
         .then(response=>{
-            // console.log(response.data.mypost);
+            console.log(response.data.posts);
             setData(response.data.posts);
             // console.log(data);
         })
     },[])
+
+
+    const LikePost = (id)=>{
+        const authAxios = Axios.create({
+            baseURL : 'http://localhost:3001',
+            headers :{
+                Authorization : `Bearer ${localStorage.getItem("jwt")}`
+            }
+        });
+        authAxios.put('/like',{
+            postId : id
+        }).then(response=>{
+            // console.log(response.data);
+            const newData = data.map(item=>{
+                if(item._id == response.data._id){
+                    return response.data
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+        }).catch(err=>console.log(err))
+    }
+
+     const UnLikePost = (id)=>{
+        const authAxios = Axios.create({
+            baseURL : 'http://localhost:3001',
+            headers :{
+                Authorization : `Bearer ${localStorage.getItem("jwt")}`
+            }
+        });
+        authAxios.put('/unlike',{
+            postId : id
+        }).then(response=>{
+            // console.log(response);
+            const newData = data.map(item=>{
+                if(item._id == response.data._id){
+                    return response.data
+                }else{
+                    return item
+                }
+            })
+            setData(newData)
+            
+        }).catch(err=>console.log(err))
+    }
 
     return(
         <div className="home">
@@ -34,6 +82,17 @@ const Home = () =>{
                             </div>
                             <div className="card-content">
                                 <i className="material-icons like" style={{color : "red"}} >favorite</i>
+                                {item.likes.includes(state._id)
+                                ? 
+                                <i className="material-icons"
+                                    onClick={()=>{UnLikePost(item._id)}}
+                                >thumb_down</i>
+                                : 
+                                <i className="material-icons"
+                                onClick={()=>{LikePost(item._id)}}
+                                >thumb_up</i>
+                                }
+                                <h6> {item.likes.length} likes </h6>
                                 <h6>{item.title}</h6>
                                 <p> {item.caption} </p>
                                 <input type="text" placeholder='Leave a comment' />
